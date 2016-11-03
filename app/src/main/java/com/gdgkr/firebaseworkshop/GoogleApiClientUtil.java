8f9b110ff60361cc5061c61406053010eaef0b54
Chansuk Yang <chansuk@google.com>
2016-11-03 15:13:47 +0900

    Added GoogleApiClientUtil to easy access of GoogleApiClient

---
+++ app/src/main/java/com/gdgkr/firebaseworkshop/GoogleApiClientUtil.java
@@ -0,0 +1,36 @@
+package com.gdgkr.firebaseworkshop;
+
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by chansuk on 2016. 11. 3..
+ */
+
+public class GoogleApiClientUtil {
+
+    private static GoogleApiClient googleApiClient;
+
+    public static void initialize(
+            FragmentActivity activity, GoogleApiClient.OnConnectionFailedListener listener) {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(activity.getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(activity)
+                .enableAutoManage(activity, listener)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    public static GoogleApiClient getInstance() {
+
+        if (googleApiClient == null)
+            throw new IllegalStateException("initialize MUST be invoked before accessing this");
+
+        return googleApiClient;
+    }
+}
